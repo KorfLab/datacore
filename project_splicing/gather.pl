7b5a5e351ce8a57b40daaa8b31e9d9922da3b5cd
iankorf <ifkorf@gmail.com>
2022-02-20 23:03:34 -0800

    oops

--- project_splicing/gather.pl
+++ project_splicing/gather.pl
@@ -3,6 +3,6 @@ my $root = "../genome_celegans/build/genes";
 while (<>) {
 	next if /^#/;
 	my ($id) = split;
-	system("cp $root/$id.fa apc/$id.fa");
-	system("cp $root/$id.gff3 apc/$id.gff");
+	system("cp $root/$id.fa apc");
+	system("cp $root/$id.gff3 apc");
 }
