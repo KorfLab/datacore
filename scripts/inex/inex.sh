775765412d46c76cf89ac1609a37ca42b4534d58
AlanAloha <zby991023@gmail.com>
2022-02-24 16:06:03 -0800

    update

--- scripts/inex/inex.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-GENEPATH=~/datacore/genome_celegans/build/mini_gene
-
-for gene_dir in ${GENEPATH}/*; do
-	python3 inex.py --fa ${gene_dir}/*.fa --gff ${gene_dir}/*.gff --out ${gene_dir}/inex
-done
