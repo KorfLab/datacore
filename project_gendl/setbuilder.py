15ba35eea0533b35503312cf0d1eb42a4233079f
iankorf <ifkorf@gmail.com>
2022-05-13 22:30:41 -0700

    42

--- project_gendl/setbuilder.py
+++ project_gendl/setbuilder.py
@@ -1,6 +1,7 @@
 import gzip
 import random
 import subprocess
+import sys
 
 def get_acceptors(filename):
 	accs = []
@@ -34,18 +35,89 @@ def write_fasta(filename, name, seqs):
 			fp.write(f'{s1}{s2}{s3}\n')
 			n += 1
 
+def randomseq(size, contents='ACGT'):
+	seq = ''
+	for i in range(size):
+		seq += random.choice(contents)
+	return seq
+
+def make_negative1(seqs):
+	neg = []
+	for i in range(len(seqs)):
+		s1 = randomseq(20)
+		s2 = seqs[0][1] # either GT or AG
+		s3 = randomseq(20)
+		x = 0
+		neg.append((s1, s2, s3, x))
+	return neg
+
+def make_negative2(seqs):
+	s1seq = '' # composition of part 1
+	s3seq = '' # composition of part 2
+	for s1, s2, s3, x in seqs:
+		s1seq += s1
+		s3seq += s3
+
+	neg = []
+	for i in range(len(seqs)):
+		s1 = randomseq(20, s1seq)
+		s2 = seqs[0][1] # either GT or AG
+		s3 = randomseq(20, s3seq)
+		x = 0
+		neg.append((s1, s2, s3, x))
+	return neg
+
+def make_negative3(seqs):
+	col1 = [[] for i in range(20)]
+	col3 = [[] for i in range(20)]
+	for s1, s2, s3, x in seqs:
+		for i in range(20):
+			col1[i].append(s1[i])
+			col3[i].append(s3[i])
+
+	neg = []
+	for i in range(len(seqs)):
+		s1 = ''
+		s3 = ''
+		for j in range(20):
+			s1 += random.choice(col1[j])
+			s3 += random.choice(col3[j])
+		s2 = seqs[0][1] # either GT or AG
+		x = 0
+		neg.append((s1, s2, s3, x))
+	return neg
+
+
+#############
+# 42 nt set # 20 nt upstream and downstream of canonical GT|AG
+#############
 
 genomes = ('at', 'ce', 'dm')
 
-
-#############
-# 42 nt set # 21 nt upstream and downstream of canonical GT|AG
-#############
-
 for gen in genomes:
+	# observed
 	eie = f'eie.{gen}.txt.gz'
 	dons = get_donors(eie)
 	accs = get_acceptors(eie)
-	write_fasta(f'data/{gen}.don.fa', 'don', dons)
-	write_fasta(f'data/{gen}.acc.fa', 'acc', accs)
+	write_fasta(f'data42/{gen}.don.fa', 'don', dons)
+	write_fasta(f'data42/{gen}.acc.fa', 'acc', accs)
+
+	# negative 1 - totally random
+	nd = make_negative1(dons)
+	na = make_negative1(accs)
+	write_fasta(f'data42/{gen}.n1don.fa', 'n1don', nd)
+	write_fasta(f'data42/{gen}.n1acc.fa', 'n1acc', na)
+
+	# negative 2 - compositional but not positional
+	nd = make_negative2(dons)
+	na = make_negative2(accs)
+	write_fasta(f'data42/{gen}.n2don.fa', 'n2don', nd)
+	write_fasta(f'data42/{gen}.n2acc.fa', 'n2acc', na)
+
+	# negative 3 - compositional and positional
+	nd = make_negative3(dons)
+	na = make_negative3(accs)
+	write_fasta(f'data42/{gen}.n3don.fa', 'n3don', nd)
+	write_fasta(f'data42/{gen}.n3acc.fa', 'n3acc', na)
+
 
