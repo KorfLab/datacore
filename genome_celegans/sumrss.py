7de7f258a176bb13c1da5e48f580d1493c23b872
icacedo <icacedo@ucdavis.edu>
2022-12-12 11:02:35 -0800

    update

--- genome_celegans/sumrss.py
+++ genome_celegans/sumrss.py
@@ -1,5 +1,6 @@
 import sys
 import gzip
+import fnmatch
 
 count = 0
 with gzip.open(sys.argv[1], 'rt') as fp:
@@ -10,3 +11,17 @@ with gzip.open(sys.argv[1], 'rt') as fp:
 		if f[1] != 'RNASeq_splice': continue
 		count += float(f[5])
 print(count)
+
+# count all RNASeq
+count = 0
+with gzip.open(sys.argv[1], 'rt') as fp:
+	while True:
+		line = fp.readline()
+		if line == '': break
+		f = line.split()
+		if f[5] == '.': continue
+		if isinstance(float(f[5]), float) != True: continue
+		count += float(f[5])
+print(count)
+
+
