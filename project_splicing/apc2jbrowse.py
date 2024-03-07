e5394b153086253e2d6f068a81298369652609f1
Ian Korf <ifkorf@ucdavis.edu>
2024-03-07 13:55:13 -0800

    up

--- project_splicing/apc2jbrowse.py
+++ project_splicing/apc2jbrowse.py
@@ -1,36 +1,73 @@
 import argparse
 from grimoire.genome import Reader
+from grimoire.feature import Feature
 
-parser = argparse.ArgumentParser(
-	description='create GFF for use with jbrowse')
-parser.add_argument('fasta', type=str, metavar='<fasta file>',
-	help='path to fasta file')
-parser.add_argument('gff', type=str, metavar='<gff file>',
-	help='path to gff file')
+parser = argparse.ArgumentParser(description='create GFF for use with jbrowse')
+parser.add_argument('fasta')
+parser.add_argument('gff')
+parser.add_argument('--emm')
+parser.add_argument('--elen')
+parser.add_argument('--imm')
+parser.add_argument('--ilen')
+parser.add_argument('--don')
+parser.add_argument('--acc')
+
 arg = parser.parse_args()
 
 genome = Reader(gff=arg.gff, fasta=arg.fasta)
 dna = next(genome)
 
 # get original genomic location from fasta defline
-loc, strand, gene = dna.desc.split()
+loc, STR, gene = dna.desc.split()
 chrom, coords = loc.split(':')
-beg, end = coords.split('-')
-beg = int(beg)
-end = int(end)
-#print(chrom, beg, end, strand)
+BEG, END = coords.split('-')
+BEG = int(BEG)
+END = int(END)
 
-# create splice acceptor features
-# create splice donor features
-# create exon features
-# create intron features
+# create gff components
+exos = []
+ints = []
+accs = []
+dons = []
+for f in dna.ftable.features:
+	coor = (f.beg, f.end)
+	if f.type == 'intron':
+		if coor not in ints: ints.append(coor)
+		if f.beg not in dons: dons.append(f.beg)
+		if f.end not in accs: accs.append(f.end)
+	elif f.type == 'exon':
+		if coor not in exos: exos.append(coor)
+
+gffs = []
+for beg, end in exos:
+	gffs.append(Feature(dna, beg, end, STR, 'exon', source='model',
+		score=0))
+for beg, end in ints:
+	gffs.append(Feature(dna, beg, end, STR, 'intron', source='model',
+		score=0))
+for beg in dons:
+	gffs.append(Feature(dna, beg, beg+1, STR, 'donor', source='model',
+		score=0))
+for beg in dons:
+	gffs.append(Feature(dna, beg, beg+1, STR, 'acceptor', source='model',
+		score=0))
+for f in gffs:
+	dna.ftable.add_feature(f)
+
 
 # convert all features to negative strand as needed
-if strand == '-': dna.revcomp()
+if STR == '-': dna.revcomp()
 
 # output all features upscaled to genome coordinates
 for f in dna.ftable.features:
+	col9 = ''
+	if f.id: col9 = f'ID={f.id}'
+	if f.pid:
+		pids = ','.join(f.pid)
+		if col9: col9 += f';Parent={pids}'
+		else:    col9 = f'Parent={pids}'
+
 	print('\t'.join((chrom, f.source, f.type,
-		str(f.beg + beg), str(f.end + beg), # this is probably off by 1
-		str(f.score), f.strand, '.')))
+		str(f.beg + BEG), str(f.end + BEG), # this is probably off by 1
+		str(f.score), f.strand, '.', col9)))
 
